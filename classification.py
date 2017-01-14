40a761339f1190605aec4a7c8b68c015d82a7c9f
Leon van Wissen <leon@vwissen.nl>
2017-01-15 00:59:43 +0100

    update

--- classification.py
+++ classification.py
@@ -1,1 +1,6 @@
 from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import f1_score
+
--- main.py
+++ main.py
@@ -2,13 +2,22 @@ import os
 import csv
 import json
 import dateutil.parser
+import numpy as np
 from collections import defaultdict
 from bs4 import BeautifulSoup
+import logging as log
+import pickle
+
+from classification import *
 
 TRAINFOLDER = 'data/train/'
-LABELFILE = 'data/sbs16mining-linking-training-labels.csv'
+LABELFILETRAIN = 'data/sbs16mining-linking-training-labels.csv'
+# LABELFILETEST = 'data/sbs16mining-linking-test-labels-librarything.csv'
+LABELFILETEST = 'data/sbs16mining-linking-training-labels.csv'
 METAFILE = 'data/sbs16mining.book-metadata.json'
-TESTFOLDER = 'data/test/'
+TESTFOLDER = 'data/train/'
+
+
 
 def main():
     """
@@ -22,6 +31,8 @@ def threadparser(location):
 
     filenames = os.listdir(location)
     messages = []
+
+    print("Parsing files in {}".format(location))
 
     for file in filenames:
         threadid = file.split('.')[1].split('.')[0]
@@ -39,17 +50,19 @@ def threadparser(location):
                 if 'Edited: ' in date:
                     date = date.replace('Edited: ', '')
                 date = dateutil.parser.parse(date)
+                date = date.isoformat()
 
                 messagedict = {
                     "postid": int(postid),
                     "threadid": int(threadid),
-                    "username": username,
-                    "date": date,
-                    "text": text
+                    # "username": username,
+                    # "date": date,
+                    # "text": text
                 }
 
                 messages.append(messagedict)
 
+    print("Parsed {} messages.".format(len(messages)))
     return messages
 
 def labelparser(filename):
@@ -64,6 +77,8 @@ def metaparser(filename):
 
     meta = json.load(filename)
 
+
+
 def bookmatcher(message, labeldata):
     """
     Finds bookid for each message (training).
@@ -74,15 +89,91 @@ def bookmatcher(message, labeldata):
     threadid = message['threadid']
     postid = message['postid']
 
-    bookid = [d['bookid'] for d in labeldata if d['postid'] == postid and d['threadid'] == threadid][0]
+    bookids = tuple(d['bookid'] for d in labeldata if int(d['postid']) == postid and int(d['threadid']) == threadid)
 
-    return bookid
+    return bookids
 
+def labeler(data, labeldata):
+    y = []
+
+    for message in data:
+        bookids = bookmatcher(message, labeldata)
+
+        if bookids == []:
+            bookids = ("UNKNOWN",)
+        y.append(bookids)
+
+    return y
 
 if __name__ == "__main__":
 
-    print(threadparser('data/train/'))
-    # labeldata = labelparser('data/sbs16mining-linking-training-labels.csv')
-    # print(bookmatcher(message, labeldata))
+    # Parse messages from the .xml files
+    training_data = threadparser(TRAINFOLDER)
+    test_data = threadparser(TESTFOLDER)
+
+    # Training class data (bookids to messages)
+    labeldata_train = labelparser(LABELFILETRAIN)
+    labeldata_test = labelparser(LABELFILETEST)
+    
+    trainkeys = [i["bookid"] for i in labeldata_train]
+    testkeys = [i["bookid"] for i in labeldata_test]
+    print(set(trainkeys).intersection(set(testkeys)))
+
+    # Build y
+    y = labeler(training_data, labeldata_train)
     #
     # metaparser('sbs16mining.book-metadata.json')
+
+    print("Classify!")
+
+    training_data = training_data
+    test_data = test_data
+
+    # Multi Label Classification
+    mlb = MultiLabelBinarizer()
+    yt = mlb.fit_transform(y)
+
+    v = DictVectorizer(sparse=True)
+    X = v.fit_transform(training_data)
+
+    clf = OneVsRestClassifier(SVC(verbose=2))
+    clf.fit(X, yt)
+
+    with open('model.pickle', 'wb') as picklefile:
+        pickle.dump(clf, picklefile)
+
+    print()
+    print("Predicting...")
+
+    # v = DictVectorizer(sparse=False)
+    X_test = v.transform(test_data)
+    yt_test = clf.predict(X_test)
+
+    yt_test = np.array(yt_test)
+    y_test = mlb.inverse_transform(yt_test)
+    # yt_test = np.expand_dims(yt_test, axis=0).transpose()
+    # print(np.shape(yt_test))
+    
+
+    # print(mlb.inverse_transform(yt))
+    # print(y)
+
+    # print(y)
+    # print(y_test)
+
+    y_true = labeler(test_data, labeldata_test)
+    y_true = mlb.transform(y_true)
+    y_true = np.array(y_true)
+    # y_true = np.expand_dims(y_true, axis=0).transpose()
+
+    y_pred = yt_test
+    # print(mlb.inverse_transform(y_true))
+
+
+
+    print('true', y_true)
+    print('pred', y_pred)
+
+    f1 = f1_score(y_true, y_pred, average='samples')
+
+    print("f1-score: {}".format(f1))
